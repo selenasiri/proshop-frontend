83750830a6a7acee8f065521dbd053f3e97fb64c
Selena Siri <selenasiri@gmail.com>
2020-11-15 19:44:03 -0500

    product screen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link, useParams  } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 const ProductScreen = () => {
+  const {id} = useParams()
+  const product = products.find(p => p._id === id)
+
+  console.log(product)
+
   return (
-    <div>
-      Product
-    </div>
+    <>
+      <Link className="btn btn-light my-3" to="/">Go Back</Link>
+    </>
   )
 }
 
