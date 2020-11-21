ef939426b175062053871c8f6554125f02142bdb
Selena Siri <selenasiri@gmail.com>
2020-11-21 12:31:01 -0500

    action and reducer

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,7 +1,5 @@
 import Axios from 'axios'
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from '../constants/userConstants'
-
-import { USER_LOGIN_REQUEST } from '../constants/userConstants'
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
   try {
--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGIN_FAIL } from "../constants/userConstants"
+import { USER_LOGIN_SUCCESS, USER_LOGIN_REQUEST, USER_LOGIN_FAIL, USER_LOGOUT } from "../constants/userConstants"
 
 export const userLoginReducer = (state = { product: { reviews: [] } }, action) => {
   switch(action.type) {
